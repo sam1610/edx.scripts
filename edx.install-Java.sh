634c0382b24f90a703da9ce2a58f97881fbb4309
SAM.AM <samir.amri@gmail.com>
2018-10-13 12:23:52 +0300

    Java-OracleJdk-Install

---
+++ edx.install-Java.sh
@@ -0,0 +1,4 @@
+#install Oracle Java 11 in Ubuntu or Linux Mint (64bit only) you can use the commands below:
+sudo add-apt-repository ppa:linuxuprising/java
+sudo apt update
+sudo apt install oracle-java11-installer
